625ee4ba34f4831416ac70fac3abc2e8872308b5
arshad-shah <shaharshad57@gmail.com>
2022-12-01 15:29:40 +0000

    updated API url

---
+++ app/src/main/java/com/arshadshah/nimaz/ui/components/bLogic/compass/Dial.kt
@@ -0,0 +1,10 @@
+package com.arshadshah.nimaz.ui.components.bLogic.compass
+
+import androidx.compose.runtime.Composable
+import com.arshadshah.nimaz.ui.components.ui.compass.DialUI
+
+@Composable
+fun Dial()
+{
+	DialUI()
+}
--- app/src/main/java/com/arshadshah/nimaz/ui/screens/QiblaScreen.kt
+++ app/src/main/java/com/arshadshah/nimaz/ui/screens/QiblaScreen.kt
@@ -3,9 +3,11 @@ package com.arshadshah.nimaz.ui.screens
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.arshadshah.nimaz.ui.components.bLogic.compass.Dial
 
 @Composable
 fun QiblaScreen(paddingValues : PaddingValues)
 {
 	Text(text = "Qibla Screen")
+	Dial()
 }
