b73f36b15122999f8eead2eaa7a8d02e5e839f1a
arshad-shah <shaharshad57@gmail.com>
2022-12-31 13:24:20 +0000

    local database setup

---
+++ app/src/main/java/com/arshadshah/nimaz/data/local/models/LocalJuz.kt
@@ -0,0 +1,13 @@
+package com.arshadshah.nimaz.data.local.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Juz")
+data class LocalJuz (
+    @PrimaryKey
+    val number: Int,
+    val name: String,
+    val tname: String,
+    val juzStartAyaInQuran: Int,
+)
