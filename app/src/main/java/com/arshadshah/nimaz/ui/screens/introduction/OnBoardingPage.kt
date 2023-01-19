e9ba3e462f85702134566438c5897e9d8996342a
arshad-shah <shaharshad57@gmail.com>
2023-01-19 17:58:40 +0000

    cleanup of intro

--- app/src/main/java/com/arshadshah/nimaz/ui/screens/introduction/OnBoardingPage.kt
+++ app/src/main/java/com/arshadshah/nimaz/ui/screens/introduction/OnBoardingPage.kt
@@ -3,6 +3,7 @@ package com.arshadshah.nimaz.ui.screens.introduction
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -12,17 +13,24 @@ import android.os.PowerManager
 import android.provider.Settings
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
+import com.arshadshah.nimaz.activities.Introduction
+import com.arshadshah.nimaz.activities.MainActivity
 import com.arshadshah.nimaz.ui.components.bLogic.settings.state.rememberPreferenceBooleanSettingState
 import com.arshadshah.nimaz.ui.components.ui.icons.Prayer
 import com.arshadshah.nimaz.ui.components.ui.settings.SettingsSwitch
@@ -37,105 +45,130 @@ sealed class OnBoardingPage(
 	val extra : @Composable () -> Unit = {} ,
 						   )
 {
-
 	object First : OnBoardingPage(
 			image = Icons.Prayer ,
-			title = "Nimaz" ,
+			title = "Assalamu alaikum" ,
 			description = "Nimaz is a muslim lifestyle companion app that helps you keep track of your daily prayers." ,
 								 )
 
 	object Second : OnBoardingPage(
 			image = FeatherIcons.Clock ,
 			title = "Prayer Times" ,
-			description = "Accurate prayer times for your location. You can also customize the prayer times." ,
+			description = "Accurate prayer times for your location, Adhan notifications, and more." ,
 								  )
 
 	object Third : OnBoardingPage(
-			image = FeatherIcons.Compass ,
-			title = "Qibla Direction" ,
-			description = "Accurately find the direction of the Qibla with Nimaz." ,
-								 )
-
-	object Fourth : OnBoardingPage(
 			image = FeatherIcons.BookOpen ,
 			title = "Quran" ,
-			description = "Read the Quran in Arabic with urdu and english translations." ,
+			description = "Quran with urdu and english translations." ,
 								  )
 
 	//the Notification permission page
 	@RequiresApi(Build.VERSION_CODES.TIRAMISU)
-	object Fifth : OnBoardingPage(
+	object Fourth : OnBoardingPage(
 			image = FeatherIcons.BookOpen ,
 			title = "Notifications" ,
-			description = "Nimaz can send you notifications for your prayers. You can also customize the notifications." ,
+			description = "Enable Notifications for Nimaz to get Prayer alerts in the form of Adhan." ,
 			extra = {
-				val context = LocalContext.current
-				//ask for notification permission
-				val permissionGranted = ActivityCompat.checkSelfPermission(
-						context ,
-						Manifest.permission.ACCESS_NOTIFICATION_POLICY
-																		  ) == PackageManager.PERMISSION_GRANTED
-				//get shared preference
-				val sharedpref = PrivateSharedPreferences(context)
-				//the state of the switch
-				val state =
-					rememberPreferenceBooleanSettingState("notificationAllowed" , permissionGranted)
-				SettingsSwitch(
-						state = state ,
-						onCheckedChange = {
-							if (it)
-							{
-								ActivityCompat.requestPermissions(
-										context as Activity ,
-										arrayOf(Manifest.permission.ACCESS_NOTIFICATION_POLICY) ,
-										1
-																 )
-							} else
-							{
-								//if its unchecked, then we need to remove the notification permission
-								//and remove the value from the shared preferences
-								sharedpref.removeData("notificationAllowed")
-							}
-						} ,
-						title = {
-							Text(text = "Allow Notifications")
-						} ,
-						subtitle = {
-							//if the permission is granted, show a checkmark and text saying "Allowed"
-							if (permissionGranted)
-							{
-								Row {
-									Icon(
-											imageVector = Icons.Filled.CheckCircle ,
-											contentDescription = "Notifications Allowed"
-										)
-									Text(text = "Allowed")
+				//if the android version 13 or above then show this
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+				{
+					val context = LocalContext.current
+					//ask for notification permission
+					val permissionGranted = ActivityCompat.checkSelfPermission(
+							context ,
+							Manifest.permission.ACCESS_NOTIFICATION_POLICY
+																			  ) == PackageManager.PERMISSION_GRANTED
+					val notificationManager =
+						context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+					//check if this apps notifications are enabled
+					val notificationEnabled = if (permissionGranted)
+					{
+
+						notificationManager.areNotificationsEnabled()
+					} else
+					{
+						false
+					}
+					//get shared preference
+					val sharedpref = PrivateSharedPreferences(context)
+					//the state of the switch
+					val state =
+						rememberPreferenceBooleanSettingState(
+								"notificationAllowed" ,
+								notificationEnabled
+															 )
+					SettingsSwitch(
+							state = state ,
+							onCheckedChange = {
+								if (it)
+								{
+									if (! permissionGranted)
+									{
+										ActivityCompat.requestPermissions(
+												context as Activity ,
+												arrayOf(Manifest.permission.ACCESS_NOTIFICATION_POLICY) ,
+												1
+																		 )
+									} else
+									{
+										//if the permission is granted then enable the notifications for this app
+										//open the notification settings
+										val intent = Intent()
+										intent.action = "android.settings.APP_NOTIFICATION_SETTINGS"
+										intent.putExtra(
+												"android.provider.extra.APP_PACKAGE" ,
+												context.packageName
+													   )
+										context.startActivity(intent)
+									}
+								} else
+								{
+									//if its unchecked, then we need to remove the notification permission
+									//and remove the value from the shared preferences
+									sharedpref.removeData("notificationAllowed")
 								}
-							} else
-							{
-								//if the permission is not granted, show a notification icon and text saying "Not Allowed"
-								Row {
-									Icon(
-											imageVector = Icons.Filled.Close ,
-											contentDescription = "Notifications Not Allowed"
-										)
-									Text(text = "Not Allowed")
+							} ,
+							title = {
+								Text(text = "Allow Notifications")
+							} ,
+							subtitle = {
+								//if the permission is granted, show a checkmark and text saying "Allowed"
+								if (notificationEnabled)
+								{
+									Row {
+										Icon(
+												imageVector = Icons.Filled.CheckCircle ,
+												contentDescription = "Notifications Allowed"
+											)
+										Text(text = "Allowed")
+									}
+								} else
+								{
+									//if the permission is not granted, show a notification icon and text saying "Not Allowed"
+									Row {
+										Icon(
+												imageVector = Icons.Filled.Close ,
+												contentDescription = "Notifications Not Allowed"
+											)
+										Text(text = "Not Allowed")
+									}
 								}
+							} ,
+							icon = {
+								Icon(
+										imageVector = Icons.Filled.Notifications ,
+										contentDescription = "Notifications"
+									)
 							}
-						} ,
-						icon = {
-							Icon(
-									imageVector = Icons.Filled.Notifications ,
-									contentDescription = "Notifications"
-								)
-						}
-							  )
+								  )
+				}
 			}
 								 )
 
 	//the location permission page
 	@RequiresApi(Build.VERSION_CODES.TIRAMISU)
-	object Sixth : OnBoardingPage(
+	object Fifth : OnBoardingPage(
 			image = FeatherIcons.MapPin ,
 			title = "Location" ,
 			description = "Nimaz can use your location to get accurate prayer times. You can also use manual location." ,
@@ -166,7 +199,7 @@ sealed class OnBoardingPage(
 												Manifest.permission.ACCESS_FINE_LOCATION ,
 												Manifest.permission.ACCESS_COARSE_LOCATION
 											   ) ,
-										1
+										2
 																 )
 							} else
 							{
@@ -214,7 +247,7 @@ sealed class OnBoardingPage(
 	//a page to ask for the battery optimization exemption
 	@SuppressLint("BatteryLife")
 	@RequiresApi(Build.VERSION_CODES.TIRAMISU)
-	object Seventh : OnBoardingPage(
+	object Sixth : OnBoardingPage(
 			image = FeatherIcons.Battery ,
 			title = "Battery Optimization" ,
 			description = "Nimaz needs to be exempted from battery optimization to work properly." ,
@@ -284,6 +317,29 @@ sealed class OnBoardingPage(
 								)
 						}
 							  )
+			}
+								   )
+	object Seventh : OnBoardingPage(
+			image = FeatherIcons.CheckCircle ,
+			title = "Onboarding Complete" ,
+			description = "You are all set to use Nimaz. You can always change these settings later. I hope Nimaz helps you in your daily life and Kindly keep me and my family in your prayers." ,
+			extra = {
+				val context = LocalContext.current
+				//a button to navigate to the main screen
+				Button(
+						onClick = {
+							val sharedPref = PrivateSharedPreferences(context)
+							sharedPref.saveDataBoolean("isFirstInstall" , false)
+							context.startActivity(Intent(context , MainActivity::class.java))
+							//remove the activity from the back stack
+							(context as Introduction).finish()
+						} ,
+						modifier = Modifier
+							.fillMaxWidth()
+							.padding(16.dp)
+					  ) {
+					Text(text = "Get Started")
+				}
 			}
 								   )
 }
