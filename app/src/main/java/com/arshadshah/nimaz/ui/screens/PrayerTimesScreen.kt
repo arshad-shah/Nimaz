b34813a38d14485e15df5771d2592485b49ee71b
arshad-shah <shaharshad57@gmail.com>
2023-02-05 16:47:11 +0000

    Loader skeleton for quran

--- app/src/main/java/com/arshadshah/nimaz/ui/screens/PrayerTimesScreen.kt
+++ app/src/main/java/com/arshadshah/nimaz/ui/screens/PrayerTimesScreen.kt
@@ -1,13 +1,17 @@
 package com.arshadshah.nimaz.ui.screens
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.arshadshah.nimaz.constants.AppConstants
@@ -16,6 +20,10 @@ import com.arshadshah.nimaz.ui.components.bLogic.prayerTimes.DatesContainer
 import com.arshadshah.nimaz.ui.components.bLogic.prayerTimes.LocationTimeContainer
 import com.arshadshah.nimaz.ui.components.bLogic.prayerTimes.PrayerTimesList
 import com.arshadshah.nimaz.ui.components.ui.loaders.CircularLoaderCard
+import com.arshadshah.nimaz.ui.components.ui.loaders.ItemSkeleton
+import com.arshadshah.nimaz.ui.components.ui.loaders.ListSkeletonLoader
+import com.arshadshah.nimaz.ui.components.ui.loaders.loadingShimmerEffect
+import com.arshadshah.nimaz.ui.components.ui.prayerTimes.LocationTimeContainerUI
 import es.dmoral.toasty.Toasty
 
 @Composable
@@ -39,11 +47,40 @@ fun PrayerTimesScreen(
 	}
 	//log all the states
 	Log.d(AppConstants.PRAYER_TIMES_SCREEN_TAG , "state: ${state.value}")
-
 	//if its not loaded yet, show a loading screen
 	if (state.value.isLoading.value || state.value.prayerTimes.value == null)
 	{
-		CircularLoaderCard()
+		Column(
+				modifier = Modifier
+					.fillMaxSize()
+					.padding(paddingValues)
+					.padding(8.dp),
+				horizontalAlignment = Alignment.CenterHorizontally ,
+				verticalArrangement = Arrangement.SpaceEvenly
+			  ) {
+			// Calling the LocationTimeContainer composable
+			ElevatedCard(
+					modifier = Modifier
+						.padding(vertical = 8.dp , horizontal = 0.dp)
+						.height(IntrinsicSize.Max)
+						.shadow(5.dp , shape = CardDefaults.elevatedShape , clip = true)
+						) {
+				ItemSkeleton(brush = loadingShimmerEffect())
+			}
+
+			// Calling the DatesContainer composable
+			DatesContainer()
+
+			// Calling the PrayerTimesList composable
+			ElevatedCard(
+					modifier = Modifier
+						.fillMaxWidth()
+						.padding(vertical = 8.dp)
+						.shadow(5.dp , shape = CardDefaults.elevatedShape , clip = true)
+						) {
+				ListSkeletonLoader(brush = loadingShimmerEffect())
+			}
+		}
 	} else if (state.value.error.value != null)
 	{
 		//if there is an error, show an error screen
