7d80002665898af8946179921aa39673e213a9f0
arshad-shah <shaharshad57@gmail.com>
2023-02-19 15:22:48 +0000

    cleanup

--- app/src/main/java/com/arshadshah/nimaz/ui/components/bLogic/prayerTimes/RamadanCard.kt
+++ app/src/main/java/com/arshadshah/nimaz/ui/components/bLogic/prayerTimes/RamadanCard.kt
@@ -20,7 +20,8 @@ import java.time.chrono.HijrahDate
 import java.time.temporal.ChronoField
 
 @Composable
-fun RamadanCard() {
+fun RamadanCard()
+{
 	//a card that shows the time left for ramadan
 	//it should only show when 40 days are left for ramadan
 	//it should show the time left for ramadan in days, hours, minutes and seconds
@@ -28,7 +29,7 @@ fun RamadanCard() {
 
 	val today = LocalDate.now()
 	val todayHijri = HijrahDate.from(today)
-	val ramadanStart = HijrahDate.of(todayHijri[ChronoField.YEAR], 9, 1)
+	val ramadanStart = HijrahDate.of(todayHijri[ChronoField.YEAR] , 9 , 1)
 	//get date of ramadan start in gregorian
 	val ramadanStartGregorian = LocalDate.from(ramadanStart)
 	//format the date to show day and month and year
@@ -37,38 +38,45 @@ fun RamadanCard() {
 	val ramadanStartFormatted = ramadanStartGregorian.format(formatter)
 
 	val isAfterRamadanStart = todayHijri.isAfter(ramadanStart)
-	if (isAfterRamadanStart) {
-		val ramadanEnd = HijrahDate.of(todayHijri[ChronoField.YEAR], 10, 1)
-		if (todayHijri.isBefore(ramadanEnd)) {
+	if (isAfterRamadanStart)
+	{
+		val ramadanEnd = HijrahDate.of(todayHijri[ChronoField.YEAR] , 10 , 1)
+		if (todayHijri.isBefore(ramadanEnd))
+		{
 			ramadanTimeLeft.value = ramadanEnd.toEpochDay() - todayHijri.toEpochDay()
 		}
-	}else{
+	} else
+	{
 		ramadanTimeLeft.value = ramadanStart.toEpochDay() - todayHijri.toEpochDay()
 	}
 	//list of images to pick from
 	//we will pick a random image from the list
-	val imagesToShow = listOf(R.drawable.ramadan, R.drawable.ramadan2, R.drawable.ramadan3, R.drawable.ramadan4)
+	val imagesToShow =
+		listOf(R.drawable.ramadan , R.drawable.ramadan2 , R.drawable.ramadan3 , R.drawable.ramadan4)
 	//pick a random image
 	val randomImage = imagesToShow.random()
 
 	//is ramadan time left less than 40 days
 	//if yes then show the card
-	if (ramadanTimeLeft.value < 40) {
+	if (ramadanTimeLeft.value < 40)
+	{
 		//show the card
 		ElevatedCard(
 				modifier = Modifier
 					.fillMaxWidth()
-					.padding(16.dp),
+					.padding(16.dp) ,
 					) {
 			Column(
 					modifier = Modifier.padding(16.dp) ,
 					verticalArrangement = Arrangement.Center ,
 					horizontalAlignment = Alignment.CenterHorizontally
 				  ) {
-				Text(text ="Ramadan is coming soon", style = MaterialTheme.typography.titleLarge)
+				Text(text = "Ramadan is coming soon" , style = MaterialTheme.typography.titleLarge)
 				Row(
-						modifier = Modifier.fillMaxWidth().padding(top = 16.dp) ,
-						verticalAlignment = Alignment.CenterVertically,
+						modifier = Modifier
+							.fillMaxWidth()
+							.padding(top = 16.dp) ,
+						verticalAlignment = Alignment.CenterVertically ,
 						horizontalArrangement = Arrangement.SpaceBetween
 				   ) {
 					Box(
@@ -91,9 +99,15 @@ fun RamadanCard() {
 						  ) {
 						//estimated start
 						Text(text = "Estimated start")
-						Text(text = "-${ramadanTimeLeft.value} days", style = MaterialTheme.typography.headlineMedium)
+						Text(
+								text = "-${ramadanTimeLeft.value} days" ,
+								style = MaterialTheme.typography.headlineMedium
+							)
 						//todays date
-						Text(text = ramadanStartFormatted.toString(), style = MaterialTheme.typography.titleSmall)
+						Text(
+								text = ramadanStartFormatted.toString() ,
+								style = MaterialTheme.typography.titleSmall
+							)
 					}
 				}
 			}
@@ -103,6 +117,7 @@ fun RamadanCard() {
 
 @Preview
 @Composable
-fun RamadanCardPreview() {
+fun RamadanCardPreview()
+{
 	RamadanCard()
 }
