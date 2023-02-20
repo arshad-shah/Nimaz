d19a5eeb1335e0e73d937948e81e44a8cc24fc9c
arshad-shah <shaharshad57@gmail.com>
2023-02-20 22:25:52 +0000

    reverted to shared preferences data in widget

--- app/src/main/java/com/arshadshah/nimaz/utils/recievers/BootReciever.kt
+++ app/src/main/java/com/arshadshah/nimaz/utils/recievers/BootReciever.kt
@@ -5,10 +5,9 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import com.arshadshah.nimaz.constants.AppConstants
-import com.arshadshah.nimaz.data.remote.models.PrayerTimes
-import com.arshadshah.nimaz.utils.LocalDataStore
+import com.arshadshah.nimaz.utils.PrivateSharedPreferences
 import com.arshadshah.nimaz.utils.alarms.CreateAlarms
-import kotlinx.coroutines.runBlocking
+import java.time.LocalDateTime
 
 class BootReciever : BroadcastReceiver()
 {
@@ -22,17 +21,18 @@ class BootReciever : BroadcastReceiver()
 			Log.d(AppConstants.BOOT_RECEIVER_TAG , "Boot Completed or Locked Boot Completed!")
 			Log.d(AppConstants.BOOT_RECEIVER_TAG , "Resetting Alarms after BootUp!")
 
-			LocalDataStore.init(context)
-			val dataStore = LocalDataStore.getDataStore()
-			val prayertimes : PrayerTimes
-			runBlocking {
-				dataStore.getAllPrayerTimes().let {
-					prayertimes = it
-				}
-			}
+			val sharedPreferences = PrivateSharedPreferences(context)
 
-			CreateAlarms().exact(context ,
-								 prayertimes.fajr !! , prayertimes.sunrise!! , prayertimes.dhuhr!! , prayertimes.asr!! , prayertimes.maghrib!! , prayertimes.isha!!)
+			val fajr = LocalDateTime.parse(sharedPreferences.getData(AppConstants.FAJR , LocalDateTime.now().toString()))
+			val sunrise =
+				LocalDateTime.parse(sharedPreferences.getData(AppConstants.SUNRISE , LocalDateTime.now().toString()))
+			val dhuhr = LocalDateTime.parse(sharedPreferences.getData(AppConstants.DHUHR , LocalDateTime.now().toString()))
+			val asr = LocalDateTime.parse(sharedPreferences.getData(AppConstants.ASR , LocalDateTime.now().toString()))
+			val maghrib =
+				LocalDateTime.parse(sharedPreferences.getData(AppConstants.MAGHRIB , LocalDateTime.now().toString()))
+			val isha = LocalDateTime.parse(sharedPreferences.getData(AppConstants.ISHA , LocalDateTime.now().toString()))
+
+			CreateAlarms().exact(context , fajr , sunrise , dhuhr , asr , maghrib , isha)
 
 			Log.d(AppConstants.BOOT_RECEIVER_TAG , "Alarms Reset after BootUp!")
 
