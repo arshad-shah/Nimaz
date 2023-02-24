4a40acf0f0179134d1555a5e538e7c6259c8275a
arshad-shah <shaharshad57@gmail.com>
2023-02-24 19:56:31 +0000

    Improvement: tasbih row UI improved

--- app/src/main/java/com/arshadshah/nimaz/ui/components/bLogic/tasbih/TasbihRow.kt
+++ app/src/main/java/com/arshadshah/nimaz/ui/components/bLogic/tasbih/TasbihRow.kt
@@ -1,11 +1,9 @@
 package com.arshadshah.nimaz.ui.components.bLogic.tasbih
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,8 +11,12 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.arshadshah.nimaz.R
 import com.arshadshah.nimaz.ui.theme.utmaniQuranFont
 
 
@@ -32,46 +34,80 @@ fun TasbihRow(
 				.padding(8.dp)
 				.clickable(
 						//disable it if onNavigateToTasbihScreen has no implementation
-						enabled = onNavigateToTasbihScreen != null,
+						enabled = onNavigateToTasbihScreen != null ,
 						  ) {
 					if (onNavigateToTasbihScreen != null)
 					{
-						onNavigateToTasbihScreen(arabicName, englishName , translationName)
+						onNavigateToTasbihScreen(arabicName , englishName , translationName)
 					}
-						   } ,
+				} ,
 				) {
-		Column(
-				horizontalAlignment = Alignment.Start ,
-				verticalArrangement = Arrangement.Center,
-			  ) {
-			CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+		Row(
+				modifier = Modifier
+					.padding(8.dp)
+					.fillMaxWidth()
+		   ) {
+			Column(
+					modifier = Modifier
+						.fillMaxWidth()
+						.padding(4.dp)
+						.weight(0.80f),
+					horizontalAlignment = Alignment.Start ,
+					verticalArrangement = Arrangement.Center ,
+				  ) {
+				CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+					Text(
+							text = arabicName ,
+							style = MaterialTheme.typography.titleLarge ,
+							fontSize = 28.sp ,
+							fontFamily = utmaniQuranFont ,
+							modifier = Modifier
+								.padding(4.dp)
+								.fillMaxWidth() ,
+							color = MaterialTheme.colorScheme.onSurface ,
+						)
+				}
 				Text(
-						text = arabicName ,
-						style = MaterialTheme.typography.titleLarge ,
-						fontFamily = utmaniQuranFont ,
 						modifier = Modifier
 							.padding(4.dp)
 							.fillMaxWidth() ,
+						text = englishName ,
+						style = MaterialTheme.typography.titleSmall ,
+						color = MaterialTheme.colorScheme.onSurface ,
+					)
+				Text(
+						modifier = Modifier
+							.padding(4.dp)
+							.fillMaxWidth() ,
+						text = translationName ,
+						style = MaterialTheme.typography.titleSmall ,
 						color = MaterialTheme.colorScheme.onSurface ,
 					)
 			}
-			Text(
-					modifier = Modifier
-						.padding(4.dp)
-						.fillMaxWidth() ,
-					text = englishName ,
-					style = MaterialTheme.typography.titleSmall ,
-					color = MaterialTheme.colorScheme.onSurface ,
-				)
-			Text(
-					modifier = Modifier
-						.padding(4.dp)
-						.fillMaxWidth() ,
-					text = translationName ,
-					style = MaterialTheme.typography.titleSmall ,
-					color = MaterialTheme.colorScheme.onSurface ,
-				)
+			if(onNavigateToTasbihScreen != null)
+			{
+				Icon(
+						painter = painterResource(id = R.drawable.angle_small_right_icon) ,
+						contentDescription = "Navigate to chapter" ,
+						modifier = Modifier
+							.size(24.dp)
+							.fillMaxWidth()
+							.align(Alignment.CenterVertically)
+					)
+			}
 		}
 	}
 
+}
+
+@Preview
+@Composable
+fun TasbihRowPreview()
+{
+	TasbihRow(
+			englishName = "Tasbih" ,
+			arabicName = "تسبيح" ,
+			translationName = "Praise" ,
+			onNavigateToTasbihScreen = null
+			 )
 }
