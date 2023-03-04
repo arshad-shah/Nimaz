8bfdf74eb304bc6a10329d6d7b21f3a7847af9dc
arshad-shah <shaharshad57@gmail.com>
2023-03-04 13:48:35 +0000

    Note: The widget is causing null pointer exceptions when it is updated, investigate, app is stable otherwise
    Feature: Delete and edit for the tasbih created

---
+++ app/src/main/java/com/arshadshah/nimaz/ui/components/ui/trackers/GoalEditDialog.kt
@@ -0,0 +1,48 @@
+package com.arshadshah.nimaz.ui.components.ui.trackers
+
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.arshadshah.nimaz.data.remote.models.Tasbih
+import com.arshadshah.nimaz.data.remote.viewModel.TasbihViewModel
+import java.time.LocalDate
+
+@Composable
+//function to open the dialog with the tasbih data
+fun GoalEditDialog(tasbih : Tasbih , showTasbihDialog : MutableState<Boolean>)
+{
+	val context = LocalContext.current
+
+	val goal = remember {
+		mutableStateOf(tasbih.goal.toString())
+	}
+	goal.value = tasbih.goal.toString()
+
+	val viewModel = viewModel(
+			key = "TasbihViewModel" ,
+			initializer = { TasbihViewModel(context) } ,
+			viewModelStoreOwner = LocalContext.current as ComponentActivity
+							 )
+	TasbihGoalDialog(
+			onConfirm = {
+				viewModel.handleEvent(
+						TasbihViewModel.TasbihEvent.UpdateTasbihGoal(
+						Tasbih(
+								id = tasbih.id ,
+								arabicName = tasbih.arabicName ,
+								englishName = tasbih.englishName ,
+								translationName = tasbih.translationName ,
+								goal = it.toInt() ,
+								count = tasbih.count ,
+								date = LocalDate.now().toString() ,
+							  )
+																))
+			} ,
+			isOpen = showTasbihDialog ,
+			state = goal
+					)
+}
