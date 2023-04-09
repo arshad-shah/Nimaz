082194ffd9b6c9eb6a53e9ddc3de247e457aa24f
arshad-shah <shaharshad57@gmail.com>
2023-04-09 23:58:10 +0100

    Missed Prayers alarm

--- app/src/main/java/com/arshadshah/nimaz/utils/recievers/MissedPrayerReciever.kt
+++ app/src/main/java/com/arshadshah/nimaz/utils/recievers/MissedPrayerReciever.kt
@@ -12,6 +12,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.time.LocalDate
+import java.time.LocalTime
+import java.time.temporal.ChronoUnit
 
 class MissedPrayerReciever : BroadcastReceiver()
 {
@@ -46,30 +48,53 @@ class MissedPrayerReciever : BroadcastReceiver()
 			try
 			{
 				var amountMissed = 0
+				val nameOfMissedPrayers = mutableListOf<String>()
 				val tracker =
 					LocalDataStore.getDataStore().getTrackerForDate(LocalDate.now().toString())
-				if (! tracker.fajr)
-				{
-					amountMissed += 1
-				} else if (! tracker.dhuhr)
-				{
-					amountMissed += 1
-				} else if (! tracker.asr)
-				{
-					amountMissed += 1
-				} else if (! tracker.maghrib)
-				{
-					amountMissed += 1
-				} else if (! tracker.isha)
-				{
-					amountMissed += 1
-				}
+
+				Log.d(
+						AppConstants.MISSED_PRAYER_RECEIVER_TAG ,
+						"onReceived:  called and tracker is $tracker"
+					 )
+
+
+					if (!tracker.fajr)
+					{
+						amountMissed++
+						nameOfMissedPrayers.add("Fajr")
+					}
+					if (!tracker.dhuhr)
+					{
+						amountMissed++
+						nameOfMissedPrayers.add("Dhuhr")
+					}
+					if (!tracker.asr)
+					{
+						amountMissed++
+						nameOfMissedPrayers.add("Asr")
+					}
+					if (!tracker.maghrib)
+					{
+						amountMissed++
+						nameOfMissedPrayers.add("Maghrib")
+					}
+					if (!tracker.isha)
+					{
+						amountMissed++
+						nameOfMissedPrayers.add("Isha")
+					}
+
+
 				val title = if (amountMissed > 0) "$amountMissed prayers missed"
 				else "Well Done!"
 
-				//message
+				//get time left to complete missed prayers
+				val timeLeft = LocalTime.now().until(LocalTime.of(23 , 59 , 59) , ChronoUnit.MINUTES)
+
+				//create message
 				val message =
-					if (amountMissed > 0) "You missed $amountMissed prayers today. Please try to pray them before the next day."
+					if (amountMissed > 0) "You missed ${nameOfMissedPrayers.joinToString(", ")} today," +
+							"\n there is still $timeLeft minutes left to complete ${if (amountMissed > 1) "them" else "it"}"
 					else "You completed all your prayers today. Keep it up!"
 
 				NotificationHelper().createNotificationForMissedPrayer(
@@ -89,7 +114,7 @@ class MissedPrayerReciever : BroadcastReceiver()
 				val mapForLoggingError = mapOf(
 						"MissedPrayerReciever" to "Error in BootReciever: ${e.message}"
 											  )
-				FirebaseLogger.logEvent("Missed Prayer Receiver" , mapForLoggingError)
+				FirebaseLogger.logEvent("MissedPrayerReceiver" , mapForLoggingError)
 			}
 		}
 
