f27e35a23641df869601a0b6c715bebf4bbfca81
Arshad Shah <shaharshad57@gmail.com>
2023-12-27 12:22:54 +0000

    WIP: updates to widgets and so on

--- app/src/main/java/com/arshadshah/nimaz/data/local/models/LocalDateSerializer.kt
+++ app/src/main/java/com/arshadshah/nimaz/data/local/models/LocalDateSerializer.kt
@@ -1,15 +1,17 @@
 package com.arshadshah.nimaz.data.local.models
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializer
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.time.LocalDate
 
+@OptIn(ExperimentalSerializationApi::class)
 @Serializer(forClass = LocalDate::class)
 object LocalDateSerializer {
 
-    override fun serialize(encoder: Encoder, date: LocalDate) {
-        encoder.encodeString(date.toString())
+    override fun serialize(encoder: Encoder, value: LocalDate) {
+        encoder.encodeString(value.toString())
     }
 
     override fun deserialize(decoder: Decoder): LocalDate {
